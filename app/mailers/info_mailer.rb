823b5dd13f0cec28a6aae2147a5f2fdf231ecf25
John Kelleher <johnpkelleher@gmail.com>
2015-01-31 18:34:24 -0500

    updated mailer to include Patti and tech.masters.swimming

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -3,6 +3,6 @@ class InfoMailer < ActionMailer::Base
 
   def interested_info_email(info)
     @info = info
-    mail(to: ['johnpkelleher@gmail.com', 'dianamv@gmail.com'], subject: "New Interested Swimmer")
+    mail(to: ['johnpkelleher@gmail.com', 'dianamv@gmail.com', 'patti@mit.edu', 'tech.masters.swimming@gmail.com'], subject: "New Interested Swimmer")
   end
 end
